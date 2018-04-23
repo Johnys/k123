e1132ecccdb64efa7128e58652dc1884e2bd344d
Johnys <jjohnys@gmail.com>
2018-04-23 02:24:43 -0300

    Configuração inicial do projeto.

---
+++ frontend/tests/test-helper.js
@@ -0,0 +1,18 @@
+import register from 'ignore-styles'
+register(['.css', '.html']);
+const { JSDOM } = require("jsdom");
+const dom = new JSDOM('<html><head><script></script></head><body></body></html>');
+global.window = dom.window;
+global.document = dom.window.document;
+global.navigator = dom.window.navigator;
+global.Node = dom.window.Node;
+global.window.mocha = {};
+global.window.beforeEach = beforeEach;
+global.window.afterEach = afterEach;
+
+require('angular/angular');
+require('angular-mocks');
+
+global.angular = window.angular;
+global.inject = global.angular.mock.inject;
+global.ngModule = global.angular.mock.module;
