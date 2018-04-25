0c2ed81defe8f61fa4164d34d50152eb2d82a621
Johnys <jjohnys@gmail.com>
2018-04-24 22:55:29 -0300

    Alterações do desing

--- frontend/src/app/app.component.js
+++ frontend/src/app/app.component.js
@@ -1,10 +1,3 @@
-import 'jquery/dist/jquery';
-import 'popper.js/dist/umd/popper';
-import 'bootstrap/dist/js/bootstrap.min';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'ng-toast/dist/ngToast.min';
-import 'ng-toast/dist/ngToast.min.css';
-import 'ui-bootstrap4';
 import controller from './app.controller';
 import '../style/app.scss';
 import template from './app.html';
